89c310a17eab2c2f177defd99855d2e2072379be
Camilo Vargas <camilo.vargas@bdi.ox.ac.uk>
2019-09-09 13:06:38 +0100

    runner

---
+++ runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export GOOGLE_APPLICATION_CREDENTIALS="/home/cavargasru/Documents/map-osm-accessibility/map-visualization-dev-dataflow.json"
+input=$1
+while IFS= read -r line
+do
+ REGION=$line
+ COUNTRY="$(echo ${line} | cut -d"/" -f2)"
+ echo "${COUNTRY}"
+ mvn -Pdataflow-runner compile exec:java -Dexec.mainClass=uk.ac.ox.map.osm.OSMToRoadClassificationSurface -Dexec.args="--project=map-visualization-dev --stagingLocation=gs://map-osm/staging/ --inputFile=gs://map-osm/inputs/africa/cameroon/cameroon-latest.osm.bz2 --output=gs://map-osm/outputs/africa/cameroon/road-class --region=europe-west1 --workerMachineType=n1-standard-1 --maxNumWorkers=8 --numWorkers=8 --experiments=shuffle_mode=service --runner=DataflowRunner"
+done < "$input"
+
